e079fc8fca8849821051f1a1dcf6535e4e6e1790
gyuhaaa <vkvkvk2892@naver.com>
2024-10-16 02:16:51 +0900

    db setting (create table)

--- src/db_setting.mjs
+++ src/db_setting.mjs
@@ -35,14 +35,43 @@ connection.query(query, (err, results, fields) => {
 // var query = "DROP TABLE `email_log`";
 
 // var query = "DROP TABLE `token`, `offer`, `order_common`, `order_buy`, `order_sell`, `index`, `config`, `user`, `email_log`";
+// var query = "DROP TABLE `token`, `network`, `index`, `config`, `user`, `email_log`";
+
+// 네트워크 정보 테이블
+var query =
+  "CREATE TABLE network (" +
+  "chain_id INT PRIMARY KEY, " +
+  "network_name VARCHAR(30), " +
+  "chain_logo_url VARCHAR(100), " +
+  "token_logo_url VARCHAR(100), " +
+  "pointmarket_addr VARCHAR(42), " +
+  "block_explorer_url VARCHAR(100), " +
+  "rpc_url VARCHAR(100), " +
+  "coin_name VARCHAR(30), " +
+  "coin_symbol VARCHAR(10), " +
+  "coin_decimals INT, " +
+  "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+  "mod_date DATETIME " +
+  ");";
+
+connection.query(query, (err, results, fields) => {
+  if (err) {
+    console.error("쿼리 실행에 실패했습니다:", err);
+    return;
+  }
+
+  console.log("쿼리 결과:", results);
+});
 
 // 토큰 정보 테이블
 var query =
   "CREATE TABLE token (" +
   "token_id INT, " +
-  "network VARCHAR(30), " +
-  "token_name VARCHAR(100) NOT NULL, " +
+  "chain_id INT, " +
+  "token_name VARCHAR(30) NOT NULL, " +
+  "token_symbol VARCHAR(10) NOT NULL, " +
   "token_addr VARCHAR(42) NOT NULL, " +
+  "logo_url VARCHAR(100), " +
   "telegram VARCHAR(100), " +
   "X VARCHAR(100), " +
   "discord VARCHAR(100), " +
@@ -53,8 +82,10 @@ var query =
   "status INT NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME, " +
-  "PRIMARY KEY (token_id, network) " +
-  ")";
+  "PRIMARY KEY (token_id, chain_id), " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE " +
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -71,23 +102,51 @@ var query = "SELECT * from token";
 var query = "UPDATE token SET settle_time = null WHERE token_id = 1";
 var query = "SELECT * from token";
 
+// 결제 토큰 정보 테이블
+var query =
+  "CREATE TABLE exchange_token (" +
+  "token_id INT, " +
+  "chain_id INT, " +
+  "token_name VARCHAR(30) NOT NULL, " +
+  "token_symbol VARCHAR(10) NOT NULL, " +
+  "token_addr VARCHAR(42) UNIQUE NOT NULL, " +
+  "logo_url VARCHAR(100), " +
+  "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+  "mod_date DATETIME, " +
+  "PRIMARY KEY (token_id, chain_id), " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE " +
+  ");";
+
+connection.query(query, (err, results, fields) => {
+  if (err) {
+    console.error("쿼리 실행에 실패했습니다:", err);
+    return;
+  }
+
+  console.log("쿼리 결과:", results);
+});
+
 // order_common 테이블
 var query =
   "CREATE TABLE order_common (" +
   "common_id INT, " +
-  "network VARCHAR(30), " +
+  "chain_id INT, " +
   "token_id INT NOT NULL, " +
-  "collateral INT NOT NULL, " +
-  "amount INT NOT NULL, " +
+  "collateral DECIMAL(39,0) NOT NULL, " +
+  "amount BIGINT NOT NULL, " +
   "exchange_token VARCHAR(42) NOT NULL, " +
   "full_match BOOLEAN, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME, " +
-  "PRIMARY KEY (common_id, network), " +
-  "FOREIGN KEY (token_id, network) REFERENCES token(token_id, network) " +
+  "PRIMARY KEY (common_id, chain_id), " +
+  "FOREIGN KEY (token_id) REFERENCES token(token_id) " +
+  "ON UPDATE CASCADE, " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE, " +
+  "FOREIGN KEY (exchange_token) REFERENCES exchange_token(token_addr) " +
   "ON UPDATE CASCADE " +
-  "ON DELETE CASCADE " +
-  ")";
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -100,20 +159,22 @@ connection.query(query, (err, results, fields) => {
 
 // order_buy 테이블
 var query =
-  "CREATE TABLE `order_buy` (" +
+  "CREATE TABLE order_buy (" +
   "order_id INT, " +
-  "network VARCHAR(30), " +
+  "chain_id INT, " +
   "common_id INT NOT NULL, " +
   "owner VARCHAR(42) NOT NULL, " +
-  "amount INT NOT NULL, " +
+  "amount BIGINT NOT NULL, " +
   "status INT NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME, " +
-  "PRIMARY KEY (order_id, network), " +
-  "FOREIGN KEY (common_id, network) REFERENCES order_common(common_id, network) " +
+  "PRIMARY KEY (order_id, chain_id), " +
+  "FOREIGN KEY (common_id) REFERENCES order_common(common_id) " +
   "ON UPDATE CASCADE " +
-  "ON DELETE CASCADE " +
-  ")";
+  "ON DELETE CASCADE, " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE " +
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -126,20 +187,22 @@ connection.query(query, (err, results, fields) => {
 
 // order_sell 테이블
 var query =
-  "CREATE TABLE `order_sell` (" +
+  "CREATE TABLE order_sell (" +
   "order_id INT, " +
-  "network VARCHAR(30), " +
+  "chain_id INT, " +
   "common_id INT NOT NULL, " +
   "owner VARCHAR(42) NOT NULL, " +
-  "amount INT NOT NULL, " +
+  "amount BIGINT NOT NULL, " +
   "status INT NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME, " +
-  "PRIMARY KEY (order_id, network), " +
-  "FOREIGN KEY (common_id, network) REFERENCES order_common(common_id, network) " +
+  "PRIMARY KEY (order_id, chain_id), " +
+  "FOREIGN KEY (common_id) REFERENCES order_common(common_id) " +
   "ON UPDATE CASCADE " +
-  "ON DELETE CASCADE " +
-  ")";
+  "ON DELETE CASCADE, " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE " +
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -154,20 +217,22 @@ connection.query(query, (err, results, fields) => {
 var query =
   "CREATE TABLE offer (" +
   "offer_id INT, " +
-  "network VARCHAR(30), " +
+  "chain_id INT, " +
   "order_id INT, " +
-  "value INT NOT NULL, " +
+  "value DECIMAL(39,0) NOT NULL, " +
   "is_buy BOOLEAN NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME, " +
-  "PRIMARY KEY (offer_id, network), " +
-  "FOREIGN KEY (order_id, network) REFERENCES order_buy(order_id, network) " +
+  "PRIMARY KEY (offer_id, chain_id), " +
+  "FOREIGN KEY (order_id) REFERENCES order_buy(order_id) " +
   "ON UPDATE CASCADE " +
   "ON DELETE CASCADE, " +
-  "FOREIGN KEY (order_id, network) REFERENCES order_sell(order_id, network) " +
+  "FOREIGN KEY (order_id) REFERENCES order_sell(order_id) " +
   "ON UPDATE CASCADE " +
-  "ON DELETE CASCADE " +
-  ")";
+  "ON DELETE CASCADE, " +
+  "FOREIGN KEY (chain_id) REFERENCES network(chain_id) " +
+  "ON UPDATE CASCADE " +
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -182,13 +247,13 @@ connection.query(query, (err, results, fields) => {
 var query =
   "CREATE TABLE `index` (" +
   "id INT AUTO_INCREMENT PRIMARY KEY, " +
-  "table_name VARCHAR(100) NOT NULL, " +
-  "column_name VARCHAR(100) NOT NULL, " +
+  "table_name VARCHAR(30) NOT NULL, " +
+  "column_name VARCHAR(30) NOT NULL, " +
   "column_value INT NOT NULL, " +
   "description VARCHAR(100) NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME " +
-  ")";
+  ");";
 
 connection.query(query, (err, results, fields) => {
   if (err) {
@@ -228,13 +293,13 @@ var query =
   "CREATE TABLE `config` (" +
   "id INT AUTO_INCREMENT PRIMARY KEY, " +
   "old_fee_wallet VARCHAR(42) NOT NULL, " +
-  "old_fee_settle INT NOT NULL, " +
-  "old_fee_refund INT NOT NULL, " +
-  "old_pledge_rate INT NOT NULL, " +
+  "old_fee_settle DECIMAL(6,3) NOT NULL, " +
+  "old_fee_refund DECIMAL(6,3) NOT NULL, " +
+  "old_pledge_rate DECIMAL(13,10) NOT NULL, " +
   "new_fee_wallet VARCHAR(42) NOT NULL, " +
-  "new_fee_settle INT NOT NULL, " +
-  "new_fee_refund INT NOT NULL, " +
-  "new_pledge_rate INT NOT NULL, " +
+  "new_fee_settle DECIMAL(6,3) NOT NULL, " +
+  "new_fee_refund DECIMAL(6,3) NOT NULL, " +
+  "new_pledge_rate DECIMAL(13,10) NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME " +
   ")";
@@ -253,7 +318,7 @@ var query =
   "CREATE TABLE user (" +
   "user_id INT AUTO_INCREMENT PRIMARY KEY, " +
   "user_addr VARCHAR(42) NOT NULL, " +
-  "email VARCHAR(200), " +
+  "email VARCHAR(100), " +
   "is_agree BOOLEAN NOT NULL, " +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
   "mod_date DATETIME " +
@@ -273,11 +338,10 @@ var query =
   "CREATE TABLE email_log (" +
   "id INT AUTO_INCREMENT PRIMARY KEY, " +
   "user_id INT NOT NULL, " +
-  "title VARCHAR(8000) NOT NULL, " +
-  "content VARCHAR(8000) NOT NULL, " +
+  "token_id INT NOT NULL," +
   "reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, " +
-  "FOREIGN KEY (user_id) REFERENCES user(user_id) " +
-  "ON UPDATE CASCADE " +
+  "FOREIGN KEY (user_id) REFERENCES user(user_id)," +
+  "FOREIGN KEY (token_id) REFERENCES token(token_id) " +
   ")";
 
 connection.query(query, (err, results, fields) => {
